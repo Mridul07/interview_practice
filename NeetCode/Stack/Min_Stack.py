9270109f5ebc95561a179620a2bc95b65d59f78c
Mridul07 <mridulpandey99@yahoo.com>
2024-04-06 11:50:33 -0400

    solved: Min Stack Class Problem

---
+++ NeetCode/Stack/Min_Stack.py
@@ -0,0 +1,48 @@
+class MinStack(object):
+
+    def __init__(self):
+        self.stack = []
+        self.min_stack = []
+
+    def push(self, val):
+        """
+        :type val: int
+        :rtype: None
+        """
+        if not self.stack:
+            self.min_stack.append(val)
+        self.stack.append(val)
+        if val < self.min_stack[-1]:
+            self.min_stack.append(val)
+        else:
+            self.min_stack.append(self.min_stack[-1])
+        return None
+        
+    def pop(self):
+        """
+        :rtype: None
+        """
+        self.stack.pop()
+        self.min_stack.pop()
+        return None
+        
+
+    def top(self):
+        """
+        :rtype: int
+        """
+        return self.stack[-1]
+
+    def getMin(self):
+        """
+        :rtype: int
+        """
+        return self.min_stack[-1]
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
