1c5528859859c7d8591f6c76d07fa34da0a43943
Mridul07 <mridulpandey99@yahoo.com>
2024-04-06 13:00:25 -0400

    solved: Generate Parentheses Problem

---
+++ NeetCode/Stack/Generate_Parentheses.py
@@ -0,0 +1,24 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        
+        stack = []
+        res = []
+
+        def backTrack(openC, closeC):
+
+            if openC == closeC == n:
+                res.append("".join(stack))
+                return
+
+            if openC < n:
+                stack.append('(')
+                backTrack(openC + 1, closeC)
+                stack.pop()
+            
+            if closeC < openC:
+                stack.append(')')
+                backTrack(openC, closeC + 1)
+                stack.pop()
+        
+        backTrack(0, 0)
+        return res
